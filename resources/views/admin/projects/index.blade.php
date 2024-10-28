95b7247dfe4a0fc3ad3428407ff277fb6a77c94f
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-10-28 18:02:18 +0100

    Completato show

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -12,25 +12,37 @@
                     </h1>
                 </div>
             </div>
-            <table class="table">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Titolo</th>
-                    <th scope="col">Contenuto</th>
-                  </tr>
-                </thead>
-                <tbody>
-                    @foreach ($projects as $project)
-                        <tr>
-                            <th scope="row">{{ $project->id }}</th>
-                            <td>{{ $project->title }}</td>
-                            <td>{{ $project->content }}</td>
-                        </tr>
-                        
-                    @endforeach
-                </tbody>
-              </table>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Titolo</th>
+                            <th scope="col">Contenuto</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($projects as $project)
+                                <tr>
+                                    <th scope="row">{{ $project->id }}</th>
+                                    <td>{{ $project->title }}</td>
+                                    <td>{{ $project->content }}</td>
+                                    <td>
+                                        <a href="{{ route('admin.projects.show', ['project' => $project->id]) }}" class="btn btn-primary btn-sm">
+                                            Vedi
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                      </table>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
